4b07b0b1f93833cb8a1f4121a683fe9fdf5fc3da
rolandoandrade <rolandoandradefernandez@gmail.com>
2022-07-09 13:21:15 -0400

    Added genome alias

--- src/types/aliases.ts
+++ src/types/aliases.ts
@@ -1,3 +1,9 @@
-export type DefaultGenType = number[][];
+import { Chromosome } from "@/main";
+/** Default gen type used by the agents */
+export type DefaultGenType = number[];
 
+/** Default fitness type used by the agents */
 export type DefaultFitnessType = number;
+
+/** Group of chromosomes */
+export type Genome<GenType> = Chromosome<GenType>[];
